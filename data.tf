426bb9613c80dd34d5cc9b438e14ab82da5dfed1
John Diket <John.Diket@insight.com>
2022-01-12 14:05:48 -0500

    added lambda with needed data blocks and IAM role

--- data.tf
+++ data.tf
@@ -13,3 +13,7 @@ data "aws_subnet" "NBoS_subnet" {
   for_each = toset(data.aws_subnets.NBoS_subnets.ids)
   id       = each.value
 }
+
+data "aws_s3_bucket" "NBoS_bucket" {
+  bucket = "NBoS-developer-bucket.s3.amazonaws.com"
+}
