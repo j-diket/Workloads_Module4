35cadca7ba6650f98da30ba7bdcb72db0f73858c
John Diket <John.Diket@insight.com>
2022-01-19 14:48:04 -0500

    terraform fmt changes

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
-    description = ""
-    value = data.aws_vpc.NBoS_vpc.id  
+  description = ""
+  value       = data.aws_vpc.NBoS_vpc.id
 }
 
 output "public_subnets" {
-    description = "IDs for public subnets within the VPC"
-    value = data.aws_subnet.NBoS_public_subnets.ids
+  description = "IDs for public subnets within the VPC"
+  value       = data.aws_subnet.NBoS_public_subnets.ids
 }
 
 output "private_subnets" {
-    description = "IDs for private subnets within the VPC"
-    value = data.aws_subnet.NBoS_private_subnets.ids
+  description = "IDs for private subnets within the VPC"
+  value       = data.aws_subnet.NBoS_private_subnets.ids
 }
